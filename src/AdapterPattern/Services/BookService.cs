54b219e20ba75b77b440281c5a19c50367441007
Jonas Mikael Ferreira Da Silva <jonas.mikael@outlook.com>
2020-06-02 17:56:12 -0300

    Create method GenerateBooksXML.

--- src/AdapterPattern/Services/BookService.cs
+++ src/AdapterPattern/Services/BookService.cs
@@ -1,12 +1,30 @@
 using System;
+using System.Xml;
 
 namespace AdapterPattern.Services
 {
     public class BookService
     {
-        public string GetBooksXML()
-        {
-            throw new NotImplementedException();
+        public XmlDocument GetBooksXML() =>
+            GenerateBooksXML();
+
+        private XmlDocument GenerateBooksXML(){
+            string xml = @"<?xml version='1.0' encoding='UTF-8'?>
+            <root>
+            <element>
+                <Name>book1</Name>
+                <author>Jonas 1</author>
+            </element>
+            <element>
+                <Name>book2</Name>
+                <author>Jonas 2</author>
+            </element>
+            </root>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            return doc;
         }
     }
 }
