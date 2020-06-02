41ea17466d672ad3559722aca45a8e08923d100c
Jonas Mikael Ferreira Da Silva <jonas.mikael@outlook.com>
2020-06-02 00:55:18 -0300

    Create AdapterCSV class.

---
+++ src/AdapterPattern/Models/AdpaterCSV.cs
@@ -0,0 +1,13 @@
+using System;
+using AdapterPattern.Interfaces;
+
+namespace AdapterPattern.Models
+{
+    public class AdapterCSV : ITarget
+    {
+        public string GetRequesJson()
+        {
+            throw new NotImplementedException();
+        }
+    }
+} 
