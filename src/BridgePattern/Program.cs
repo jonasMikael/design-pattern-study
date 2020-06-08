858cfde4835797169c587734232bc5e95628a094
Jonas Mikael Ferreira Da Silva <jonas.mikael@outlook.com>
2020-06-08 18:22:56 -0300

    Main change to call the implementations of abstraction.

--- src/BridgePattern/Program.cs
+++ src/BridgePattern/Program.cs
@@ -6,7 +6,18 @@ namespace BridgePattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Client client = new Client();
+
+            Abstraction abstraction;
+            // The client code should be able to work with any pre-configured
+            // abstraction-implementation combination.
+            abstraction = new Abstraction(new ConcreteImplementationA());
+            client.ClientCode(abstraction);
+            
+            Console.WriteLine();
+            
+            abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+            client.ClientCode(abstraction);
         }
     }
 }
